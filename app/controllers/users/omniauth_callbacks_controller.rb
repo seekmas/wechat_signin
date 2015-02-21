47aa9f4c49563370db10be4a04e7724b646571fc
seekmas <seekmas@msn.cn>
2015-02-21 16:18:18 +0800

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,11 +3,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
         #You need to implement the method below in your model (e.g. app/models/user.rb)
         @user = User.from_omniauth(request.env["omniauth.auth"])
-        if @user.id == nil
-          @user.save
-        end
+        # if @user.id == nil
+        #   @user.save
+        # end
 
-        render :text => @user.inspect
+        render :text => @user
 
         # if @user.persisted?
         #    sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
