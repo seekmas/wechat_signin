c415c0c27a8e0c123d1c67257b6005179f076190
seekmas <seekmas@msn.cn>
2015-02-21 15:09:03 +0800

    fix

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def wechat
-        puts request.env["omniauth.auth"]
+        render :text => request.env["omniauth.auth"]
         # You need to implement the method below in your model (e.g. app/models/user.rb)
         #@user = User.from_omniauth(request.env["omniauth.auth"])
 
