61cd9d31d6eecf57669da10e02e9962db620dfbd
seekmas <seekmas@msn.cn>
2015-02-21 14:58:03 +0800

    fix auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,12 @@ class User < ActiveRecord::Base
          :omniauthable, :omniauth_providers => [:wechat]
 
   def self.from_omniauth(auth)
+      puts auth
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-          user.email = auth.info.email
-          user.password = Devise.friendly_token[0,20]
-          user.name = auth.info.name   # assuming the user model has a name
-          user.image = auth.info.image # assuming the user model has an image
+          user.email = auth.uid+'@wechat.com'
+          user.password = Devise.friendly_token[0,6]
+          user.nickname = auth.info.nickname   # assuming the user model has a name
+          user.headimgurl = auth.info.headimgurl # assuming the user model has an image
       end
   end
 
