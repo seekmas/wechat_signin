aad44f0a19d23b48c70b90a4dfa1745c745ee4e1
seekmas <seekmas@msn.cn>
2015-02-21 15:31:59 +0800

    fix

--- Gemfile
+++ Gemfile
@@ -12,4 +12,5 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 gem 'spring',        group: :development
 gem 'devise'
+gem 'omniauth-oauth2'
 gem "omniauth-wechat-oauth2"
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,9 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def wechat
-        render :text => request.env["omniauth.auth"].inspect
+
+        render :text => request.env["omniauth.auth"][:extra][:info][:uid].inspect
+
+
         # You need to implement the method below in your model (e.g. app/models/user.rb)
         #@user = User.from_omniauth(request.env["omniauth.auth"])
 
