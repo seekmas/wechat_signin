05aaec05d14eeebe3da0b3d7a32f19aade540530
seekmas <seekmas@msn.cn>
2015-02-21 14:45:43 +0800

    add all

---
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -0,0 +1,14 @@
+class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def wechat
+        # You need to implement the method below in your model (e.g. app/models/user.rb)
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+
+        if @user.persisted?
+            sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+            set_flash_message(:notice, :success, :kind => "Wechat") if is_navigational_format?
+        else
+            session["devise.wechat_data"] = request.env["omniauth.auth"]
+            redirect_to new_user_registration_url
+        end
+    end
+end
